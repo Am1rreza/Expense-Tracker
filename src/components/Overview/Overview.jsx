63abf0a4c430413b5ce4552129a522327d408300
Amirreza <amirreza.m13825@gmail.com>
2023-01-25 15:44:27 +0330

    transaction styles added

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -57,7 +57,7 @@ const TransactionForm = ({ setIsShow, addTransaction }) => {
 
     setFormValues({
       type: "expense",
-      amount: 0,
+      amount: "",
       description: "",
     });
   };
--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,14 +1,26 @@
+import styles from "./transaction.module.css";
+
 const Transaction = ({ transaction }) => {
   return (
     <section>
-      {transaction.map((t) => {
-        return (
-          <div key={t.id}>
-            <p>{t.description}</p>
-            <p>{t.amount}</p>
-          </div>
-        );
-      })}
+      {transaction.length === 0 ? (
+        <h3 style={{ padding: "20px 0 10px 0", textAlign: "center" }}>
+          Add Transaction !
+        </h3>
+      ) : (
+        transaction.map((t) => {
+          return (
+            <div
+              key={t.id}
+              className={styles.transaction}
+              style={{ borderRight: t.type === "expense" && "4px solid red" }}
+            >
+              <span>{t.description}</span>
+              <span>{t.amount}$</span>
+            </div>
+          );
+        })
+      )}
     </section>
   );
 };
