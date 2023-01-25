ef4a015a4ab27921d0262b62927db9bfa60c323a
Amirreza <amirreza.m13825@gmail.com>
2023-01-25 16:37:35 +0330

    search box added

--- src/components/Transaction/Transaction.jsx
+++ src/components/Transaction/Transaction.jsx
@@ -1,14 +1,59 @@
+import { useEffect, useState } from "react";
 import styles from "./transaction.module.css";
 
 const Transaction = ({ transaction }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredTransactions, setFilteredTransactions] = useState(transaction);
+
+  useEffect(() => {
+    filterTransactions(searchValue);
+  }, [transaction]);
+
+  // Handlers
+  const filterTransactions = (searchItem) => {
+    if (!searchItem || searchItem === "") {
+      setFilteredTransactions(transaction);
+      return;
+    }
+
+    const filtered = transaction.filter((t) =>
+      String(t.description).toLowerCase().includes(searchItem.toLowerCase())
+    );
+
+    setFilteredTransactions(filtered);
+  };
+
+  const changeHandler = (e) => {
+    setSearchValue(e.target.value);
+    filterTransactions(e.target.value);
+  };
+
+  // Conditional rendering
+  if(!transaction.length)
+    return (
+      <h3 style={{ padding: "20px 0 10px 0", textAlign: "center" }}>
+        Add Transaction !
+      </h3>
+    );
+
   return (
     <section>
-      {transaction.length === 0 ? (
+      <div className={styles.searchBox}>
+        <input
+          type="text"
+          placeholder="Search Transaction..."
+          value={searchValue}
+          onChange={changeHandler}
+        />
+      </div>
+
+      {/* loop on transactions */}
+      {filteredTransactions.length === 0 ? (
         <h3 style={{ padding: "20px 0 10px 0", textAlign: "center" }}>
-          Add Transaction !
+          No Item Matches !
         </h3>
       ) : (
-        transaction.map((t) => {
+        filteredTransactions.map((t) => {
           return (
             <div
               key={t.id}
